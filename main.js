7b869bf7a94c8c6a8db9038ab2de5f67fa653146
virgil elliot <115853295+vejtheguy@users.noreply.github.com>
2025-12-24 12:31:47 -0800

    update: set grid size with i nput

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
+const containerSize = 600;
 const container = document.getElementById("container");
-const columns = 16;
-const grid = columns * columns;
-const containerSize = 900;
-const squareSize = containerSize / columns;
+const getGridSize = document.getElementById("gridSize");
+const resizeBtn = document.getElementById("resizeBtn");
+const gridSizeNum = document.getElementById("gridSizeNum");
+const defaultSize = getGridSize.value;
 
-container.style.setProperty("--box-size", `${columns}`);
+resizeBtn.addEventListener("click", () => {
+  setGrid(getGridSize.value);
+});
 
-const setGrid = () => {
-  for (let i = 1; i <= grid; i++) {
+getGridSize.addEventListener("input", (e) => {
+  const size = e.target.value;
+  gridSizeNum.textContent = `${size} x ${size}`;
+});
+
+const setGrid = (columns) => {
+  container.textContent = "";
+  container.style.setProperty("--box-size", `${columns}`);
+  for (let i = 1; i <= columns * columns; i++) {
     const newBox = document.createElement("div");
     newBox.classList.add("box");
     container.appendChild(newBox);
@@ -17,4 +27,5 @@ const setGrid = () => {
   }
 };
 
-setGrid();
+setGrid(getGridSize.value);
+gridSizeNum.textContent = `${defaultSize} x ${defaultSize}`;
